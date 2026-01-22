f6d239c0476fc12797f5aa9f4107a66ecdd1da4c
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-01-22 09:07:23 -0300

    resolução dos exercícios 099 e 100

--- mundo03/exercícios/099.py
+++ mundo03/exercícios/099.py
@@ -4,3 +4,21 @@
 # Seu programa tem que analisar todos os valores e dizer qual deles
 # é o maior
 
+def maior(*num):
+  return list(map(max, num))
+
+
+n = [[1, 2, 3, 4, 5],[6, 7, 8, 9],[10, 11, 12], [13, 14], [15]]
+c = 0
+
+print("-" * 40)
+while c < len(n):
+  print("Analizando os valores passados...")
+  for v in n[c]:
+    print(v, end=" ")
+  print()
+  print(f"Foram informados {len(n[c])} valores ao todo.")
+  print(f"O maior valor informado foi {maior(n[c])}")
+  print("-" * 40)
+  c += 1
+  
--- mundo03/exercícios/100.py
+++ mundo03/exercícios/100.py
@@ -5,3 +5,25 @@
 # vai mostrar a soma entre todos os valores PARES sorteados pela
 # função anterior.
 
+from random import randint
+
+numeros_sorteados = []
+
+def sorteia():
+  while len(numeros_sorteados) < 5:
+    numeros_sorteados.append(randint(1, 100))
+
+
+def somaPar():
+  pares = 0
+  for v in numeros_sorteados:
+    if v % 2 == 0:
+      pares += v
+  return pares
+
+
+sorteia()
+
+print(f"Os números sorteados foram: {numeros_sorteados}")
+print(f"A soma dos números pares é: {somaPar()}")
+
