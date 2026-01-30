c563a069b513e477c61f01d839e9b8530ac52521
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-01-30 15:00:32 -0300

    resolvendo o exercício 105

--- mundo03/exercícios/105.py
+++ mundo03/exercícios/105.py
@@ -8,3 +8,33 @@
 # - A situação (opcional)
 # Adicione também as docstrings da função.
 
+def notas(*n, sit=False):
+  notas = []
+  c = 0
+  for numeros in n:
+    notas.append(numeros)
+    c += numeros
+  quantidade = len(notas)
+  maior = max(notas)
+  menor = min(notas)
+  media = c / quantidade
+  dicionario = {"total": quantidade,
+                "maior": maior,
+                "menor": menor,
+                "média": f"{media:.2f}"}
+  if sit:
+    if media >= 7:
+      dicionario["situação"] = "BOA"
+      return dicionario
+    if media >= 5:
+      dicionario["situação"] = "RAZOÁVEL"
+      return dicionario
+    else:
+      dicionario["situação"] = "RUIM"
+      return dicionario
+  else:
+     return dicionario
+
+
+print(notas(4,5,9,7,2,4, sit=True))
+
