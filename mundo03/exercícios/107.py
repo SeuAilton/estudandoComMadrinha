e126df74696d7dbf0c40d537b49aa08584faedff
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-02-28 15:53:48 -0300

    Adicionando a aula 07 e os exercícios propostos

---
+++ mundo03/exercícios/107.py
@@ -0,0 +1,5 @@
+# DESAFIO 107
+# Crie um módulo chamdo moeda.py que tenha as funções que tenha as funções
+# incorporadas aumentar(), diminuir(), dobro() e metade().
+# Faça também um programa que importe esse módulo e use algumas dessas funções.
+
