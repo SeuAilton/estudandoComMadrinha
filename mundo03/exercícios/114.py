ad84f26365a29753217bf7f50c5afa0f381df15a
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-03-09 15:13:18 -0300

    resolução do exercício 114

--- mundo03/exercícios/114.py
+++ mundo03/exercícios/114.py
@@ -1,4 +1,17 @@
 # DESAFIO 114
 # Crie um código em Python que teste se o site Pudim está acessível
 # pelo computador usado.
+import requests
+
+def estaNoAr(url):
+  try:
+    response = requests.get(url)
+    if response.status_code == 200:
+      print("\033[0;32mSucesso! O site está no ar.\033[m")
+  except:
+    print("\033[0;31mSite não encontrado.\033[m")
+
+
+estaNoAr("https://pudim.com.br/")
+#estaNoAr("")
 
