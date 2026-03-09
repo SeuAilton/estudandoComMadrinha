9a59ec636b277d8ff88f93d4c3a3ed9be4edef0f
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-03-09 16:26:58 -0300

    refatorando o exrecício 114

--- mundo03/exercícios/114.py
+++ mundo03/exercícios/114.py
@@ -6,12 +6,20 @@ import requests
 def estaNoAr(url):
   try:
     response = requests.get(url)
-    if response.status_code == 200:
-      print("\033[0;32mSucesso! O site está no ar.\033[m")
-  except:
+    response.raise_for_status()
+  except requests.HTTPError as http_error:
     print("\033[0;31mSite não encontrado.\033[m")
+    print(f"\033[0;31mERRO HTTP: {http_error}[m")
+  except requests.Timeout as time_err:
+    print("\033[0;31mNão foi possível conectar ao site.\033[m")
+    print(f"\033[0;31mERRO Timeout: {time_err}[m")
+  except requests.RequestException as error:
+    print("\033[0;31mSite não encontrado.\033[m")
+    print(f"\033[0;31mERRO: {error}\033[m")
+  else:
+    print("\033[0;32mSucesso! O site está no ar.\033[m")
 
 
 estaNoAr("https://pudim.com.br/")
-#estaNoAr("")
+estaNoAr("https://madrinha.com.br")
 
