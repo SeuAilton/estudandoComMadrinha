d8d303d05cb9d17947af353b6019921d005d014d
Preludio <123329775+SeuAilton@users.noreply.github.com>
2026-03-10 20:16:11 -0300

    resolução do exercício 115

--- mundo03/exercícios/115.py
+++ mundo03/exercícios/115.py
@@ -3,4 +3,7 @@
 # nome e idade em um arquivo de texto simples.
 # O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as
 # pessoas cadastradas.
+from cadastros import menu
+
+menu()
 
--- mundo03/exercícios/cadastros.py
+++ mundo03/exercícios/cadastros.py
@@ -3,29 +3,33 @@ def adicionar():
   while True:
     try:
       idade = int(input("Idade: ").strip())
-      if idade:
+      if idade > 0 and idade < 100:
         break
+      else:
+        print("\033[0;31m[ERRO]Digite uma idade válida!\033[m")       
     except ValueError:
       print("\033[0;31m[ERRO]Digite uma idade válida!\033[m")
-  user = {}
-  user["nome"] = nome
-  user["idade"] = idade
-  dicionario.append(user)
-# TODO: Os itens do dicionário estão sendo substituidos
-# pensar em uma forma de ir adicionando vários à lista
+  dicionario["nome"] = nome
+  dicionario["idade"] = idade
+  pessoas.append(dicionario.copy())
+  dicionario.clear()
+
 
 def menu():
   while True:
-    print(f"""{'-' * 20}
-Menu Principal
-{'-' * 20}
-1 - Ver pessoas cadastradas
-2 - Cadastrar nova Pessoa
-3 - Sair do sistema
+    print()
+    print(f"""{'-' * 30}
+        Menu Principal
+{'-' * 30}
+\033[0;33m1\033[m - \033[0;34mVer pessoas cadastradas\033[m
+\033[0;33m2\033[m - \033[0;34mCadastrar nova Pessoa\033[m
+\033[0;32m3\033[m - \033[0;34mSair do sistema\033[m
+{'-' * 30}
 """)
     while True:
       try:
-        escolha = int(input("Escolha uma opção: ").strip())
+        escolha = int(input("\033[0;35mEscolha uma opção\033[m: ").strip())
+        print()
         if escolha < 4 and escolha > 0:
           break
         else:
@@ -33,18 +37,21 @@ Menu Principal
       except Exception:
         print("\033[0;31m[ERRO]Digite uma opção válida!\033[m")
     if escolha == 1:
-      # TODO: Verificar se o dicionário está vazio e se tiver adicionar
-      # uma mensagem adequada
-      for index in range(len(dicionario)):
-        for key in dicionario[index]:
-          print(dicionario[index][key])
+      if len(pessoas) == 0:
+        print("\033[0;33mSem pessoas cadastradas!\033[m")
+      else:
+        print("-" * 30)
+        print(" \033[0;32mLista de pessoas cadastradas\033[m")
+        print("-" * 30)
+      for a in pessoas:
+        print(f"{a['nome']:.<22}", end=" ")
+        print(f"{a['idade']} anos")
     if escolha == 2:
       adicionar()
     if escolha == 3:
       break
 
 
-dicionario = [{}]
-
-menu()
+dicionario = {}
+pessoas = []
 
